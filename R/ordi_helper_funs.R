d2a17c6c6a048db0e8f4f4b354b7c71eb3d382c5
jannes.muenchow <jannes.muenchow@gfk.com>
2020-08-11 15:49:34 +0200

    putting helper_funs into R folder and fixing pull bug

--- R/ordi_helper_funs.R
+++ R/ordi_helper_funs.R
@@ -10,34 +10,37 @@
 #'   \code{"dca"}, \code{"nmds"} and \code{"isomap"}.
 #' @return Output of the ordination will be returned.
 #' @author Jannes Muenchow
+#' @importFrom vegan decorana metaMDS vegdist scores envfit
+#' @importFrom dplyr select
 
 ord_fun = function(data, env_data, method) {
   row.names(data) = data$pnr
   data$pnr = NULL
   if (method == "dca") {
-    ord = decorana(data, iweigh = TRUE)
+    ord = vegan::decorana(data, iweigh = TRUE)
     cat("single axis contribution: ", 
         round(ord$evals / sum(ord$evals), 2), "\n")
     cat("cumulative axes contribution: ",
         round(cumsum(ord$evals / sum(ord$evals)), 2))  
   }
   if (method == "nmds") {
-    ord = metaMDS(data, k = 4, try = 10)
+    ord = vegan::metaMDS(data, k = 4, try = 10)
     cat("R^2 first two axes: ", 
-        round(cor(vegdist(data), dist(scores(ord)[, 1:2]))^2, 3), "\n")
+        round(cor(vegan::vegdist(data), 
+                  dist(vegan::scores(ord)[, 1:2]))^2, 3), "\n")
     cat("R^2 first axis: ", 
-        round(cor(vegdist(data), dist(scores(ord)[, 1]))^2, 3))
+        round(cor(vegan::vegdist(data), dist(vegan::scores(ord)[, 1]))^2, 3))
   }
   
   if (method == "isomap") {
-    ord = bestisomap(vegdist(data, "bray"), k = 2)
+    ord = bestisomap(vegan::vegdist(data, "bray"), k = 2)
     cat("Cumulative explained variance (%): ", 
         ord$`Cumulative explained variance in %`)
     ord = ord$Scores
     x11()
   }
   
-  fit = envfit(ord, dplyr::select(env_data, -pnr, -id), perm = 999)
+  fit = vegan::envfit(ord, dplyr::select(env_data, -pnr, -id), perm = 999)
   plot(ord)
   plot(fit, p.max = 0.05)
   return(ord)
@@ -62,40 +65,49 @@ ord_fun = function(data, env_data, method) {
 #' @param data The dataframe containing the columns.
 #' @return xyplot
 #' @author Jannes Muenchow
+#' @importFrom rlang as_string enexpr
+#' @importFrom dplyr filter pull
+#' @importFrom lattice xyplot panel.text panel.points panel.superpose
 
 multi_panel_plot = function(x, y, by, data, id = "id") {
   # well, I have to admit I am not a big fan of NSE, way too complicated...
   lim = range(data[, rlang::as_string(rlang::enexpr(y))]) - c(0.25, -0.5)
-  p_1 = substitute(
-    xyplot(y ~ x | by, data = data, pch = 16, col = "black", as.table = TRUE,
-           ylim = lim,
-           groups = by, cex = 0.8, layout = c(2, 2),
-           scales = list(y = "same",
-                         alternating = c(1, 0),
-                         tck = c(1, 0)),
-           strip = strip.custom(bg = "white",
-                                par.strip.text = list(cex = 1)),
-           panel = panel.superpose,
-           # normally, one would use x, y, here, we use x_2 and y_2, otherwise
-           # substitute would change x and y to the names of the ordination axes
-           # which is not what we want here
-           panel.groups = function(x_2, y_2, group.number, ...) { 
-             # first plot all x and y values
-             all_x = pull(data, x)
-             all_y = pull(data, y)
-             panel.points(all_x, all_y, col = "grey85", pch = 16)
-             # secondly, overlay them with the points of the factor
-             panel.points(x_2, y_2, ...)
-             # add labels
-             fil = dplyr::select(data, by) %>% pull %>% as.factor %>% levels
-             fil = fil[group.number]
-             lab = filter(data, by == fil) %>% pull(id)
-             # a reviewer asked us to make the numbers legible, therefore only
-             # use a subset
-             ind = which(lab %in% c(1, 3, 11, 24, 33, 47, 50))
-             panel.text(x_2[ind], y_2[ind], labels = lab[ind], cex = 1, pos = 3)
-           })
-    )
+  p_1 = 
+    substitute(
+      expr = {
+        lattice::xyplot(y ~ x | by, data = data, pch = 16, 
+                    col = "black", as.table = TRUE,
+                    ylim = lim,
+                    groups = by, cex = 0.8, layout = c(2, 2),
+                    scales = list(y = "same",
+                                  alternating = c(1, 0),
+                                  tck = c(1, 0)),
+                    strip = strip.custom(bg = "white",
+                                         par.strip.text = list(cex = 1)),
+                    panel = lattice::panel.superpose,
+                    # normally, one would use x, y, here, we use x_2 and y_2,
+                    # otherwise substitute would change x and y to the names of
+                    # the ordination axes which is not what we want here
+                    panel.groups = function(x_2, y_2, group.number, ...) { 
+                      # first plot all x and y values
+                      all_x = dplyr::pull(data, x)
+                      all_y = dplyr::pull(data, y)
+                      lattice::panel.points(all_x, all_y, col = "grey85",
+                                            pch = 16)
+                      # secondly, overlay them with the points of the factor
+                      lattice::panel.points(x_2, y_2, ...)
+                      # add labels
+                      fil = dplyr::select(data, by) %>%
+                        dplyr::pull() %>% as.factor %>% levels
+                      fil = fil[group.number]
+                      lab = dplyr::filter(data, by == fil) %>% dplyr::pull(id)
+                      # a reviewer asked us to make the numbers legible,
+                      # therefore only use a subset
+                      ind = which(lab %in% c(1, 3, 11, 24, 33, 47, 50))
+                      lattice::panel.text(x_2[ind], y_2[ind], labels = lab[ind],
+                                         cex = 1, pos = 3)
+                    })
+  }, env = environment())
   eval(p_1)
 }
 
